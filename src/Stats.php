197839ba6dbe113fcde06fd343ef463f61160004
Tim Lytle <tim@timlytle.net>
2021-12-07 10:57:31 -0500

    ✔ Passing Tests

--- src/Stats.php
+++ src/Stats.php
@@ -4,32 +4,38 @@ namespace TwelveTDD;
 
 class Stats
 {
+    /**
+     * @var int[]
+     */
+    private array $values;
+
     public static function make(int ...$values)
     {
-
+        return new self(...$values);
     }
 
     private function __construct(int ...$values)
     {
-
+        $this->values = $values;
     }
 
     public function min(): int
     {
-
+        return min($this->values);
     }
 
     public function max(): int
     {
-
+        return max($this->values);
     }
 
     public function count(): int
     {
-
+        return count($this->values);
     }
 
     public function average(): float
     {
+        return array_sum($this->values) / $this->count();
     }
 }
