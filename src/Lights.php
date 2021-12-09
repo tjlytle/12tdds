50c57c5eb0034358cf901bbdfcdf372b840951ea
Tim Lytle <tim@timlytle.net>
2021-12-09 11:43:55 -0500

    ✔ Solution to Day 4

--- src/Lights.php
+++ src/Lights.php
@@ -2,6 +2,8 @@
 
 namespace TwelveTDD;
 
+use Generator;
+
 class Lights
 {
     private array $lights = [];
@@ -16,6 +18,50 @@ class Lights
         $this->set($start, $end, false);
     }
 
+    public function count(): int
+    {
+        return array_reduce(
+            $this->lights,
+            fn(int $count, array $row) => count($row) + $count,
+            0
+        );
+    }
+
+    public function sequence(): string
+    {
+        return trim(implode("", iterator_to_array($this->buildSequence())), ',');
+    }
+
+    private function buildSequence(): Generator
+    {
+        foreach ($this->lights as $y => $row) {
+            $last = null;
+
+            foreach (array_keys($row) as $pos) {
+                $last ??= $pos;
+
+                // first set
+                if ($pos === $last) {
+                    yield $pos + (1000 * $y) . '-';
+                    continue;
+                }
+
+                if ($pos === $last + 1) {
+                    // still in a set
+                    $last = $pos;
+                    continue;
+                }
+
+                yield $last + (1000 * $y) . ',';
+                yield $pos + (1000 * $y) . '-';
+            }
+
+            if (null !== $last) {
+                yield $last + (1000 * $y) . ',';
+            }
+        }
+    }
+
     private function set(Coord $start, Coord $end, bool $on)
     {
         $x = $start->getX();
@@ -23,9 +69,9 @@ class Lights
 
         do {
             if ($on) {
-                $this->lights[$x][$y] = true;
+                $this->lights[$y][$x] = true;
             } else {
-                unset($this->lights[$x][$y]);
+                unset($this->lights[$y][$x]);
             }
 
             if ($x < $end->getX()) {
@@ -36,14 +82,5 @@ class Lights
             $x = $start->getX();
             $y++;
         } while($y <= $end->getY());
-    }
-
-    public function count(): int
-    {
-        return array_reduce(
-            $this->lights,
-            fn(int $count, array $row) => count($row) + $count,
-            0
-        );
     }
 }
--- test/LightsTest.php
+++ test/LightsTest.php
@@ -9,7 +9,7 @@ class LightsTest extends TestCase
 {
     /**
      * @test
-     * @dataProvider provideRunnerAndExpected
+     * @dataProvider provideRunnerAndCount
      */
     public function count_returns_expected(callable $runner, int $expected): void
     {
@@ -19,7 +19,19 @@ class LightsTest extends TestCase
         self::assertSame($expected, $sut->count());
     }
 
-    public function provideRunnerAndExpected()
+    /**
+     * @test
+     * @dataProvider provideRunnerAndSequence
+     */
+    public function sequence_returns_expected(callable $runner, string $expected): void
+    {
+        $sut = new SUT();
+        $runner($sut);
+
+        self::assertSame($expected, $sut->sequence());
+    }
+
+    public function provideRunnerAndCount()
     {
         $input = file(__DIR__ . '/../day3/input.txt');
         $expected = file(__DIR__ . '/../day3/expected.txt');
@@ -32,6 +44,23 @@ class LightsTest extends TestCase
                     array_walk($commands, [$this, 'applyCommand'], $sut);
                 },
                 (int) $count
+            ];
+        }
+    }
+
+    public function provideRunnerAndSequence()
+    {
+        $input = file(__DIR__ . '/../day4/input.txt');
+        $expected = file(__DIR__ . '/../day4/expected.txt');
+
+        foreach ($expected as $line => $sequence) {
+            $commands = array_slice($input, 0, $line + 1);
+
+            yield [
+                function(SUT $sut) use ($commands) {
+                    array_walk($commands, [$this, 'applyCommand'], $sut);
+                },
+                (string) trim($sequence) // remove newline
             ];
         }
     }
